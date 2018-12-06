8533ef489f88534ccfd7d47c763a8dda6c3bc29a
Shravan <shravan097@users.noreply.github.com>
2018-12-05 20:30:38 -0500

    merged fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,12 +13,8 @@ import history from './history';
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-        <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
-=======
+
         <Router history={history}>
->>>>>>> studentDashboard
           <div>
             <Route exact={true} path='/' component={Home} />
             <Route exact={true} path='/signup' component={Signup} />
@@ -26,12 +22,7 @@ class App extends Component {
             <Route exact={true} path='/staff' component ={Staff}/>
             <Route exact={true} path='/student' component={StudentDashboardMain}/>
           </div>
-<<<<<<< HEAD
-          </MuiThemeProvider>
-        </BrowserRouter>
-=======
         </Router>
->>>>>>> studentDashboard
     );
   }
 }
--- frontend/src/history.js
+++ frontend/src/history.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 
 import {createBrowserHistory} from 'history'
-=======
-import { createBrowserHistory } from 'history';
->>>>>>> staffDashboard
 
 export default createBrowserHistory();
